a69f4759daad16b3df2d4a412232c18d2ec1399b
Asa Holland <asaleholland@gmail.com>
2021-11-06 23:17:54 -0700

    implement excel to json function

--- index.js
+++ index.js
@@ -1,42 +1,203 @@
-const express = require ("express");
+// references:
+// Multer guide: https://www.youtube.com/watch?v=EVOFt8Its6I
+
+
+const path = require("path");
+const express = require("express");
+
+// require multer (middleware to handle files uploaded in the body)
+const multer = require("multer");
+const cors = require("cors");
+
+// import package to handle excel to json
+'use strict';
+const excelToJson = require('convert-excel-to-json');
+
+
+// install fs (file system) to delete files off the server after we've processed them
+const fs = require("fs");
+
+
+// initial app setup
 const app = express();
 
+// debug mode
+var isDebugMode = false;
 
-// import { convertJsonToExcel } from "./xl_json";
-const EtoJ = require('./json_xl');
-const JtoE = require('./xl_json');
-
-
-// import data from a json
-const importData = require(".//data.json");
-
+// set port settings
 let port = process.env.PORT || 5151;
 
+
+// install CORS so headers can be accessed
+app.use(cors());
+
+// set file storage settings which defines out destination and filename settings
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, callback) => {
+
+    // save the file into the data folder
+    callback(null, "./data");
+  },
+  filename: (req, file, callback) => {
+
+    // use the current time to create a filename, prepended to the provided filename
+    callback(null, Date.now() + "--" + file.originalname);
+  },
+});
+
+// Route To Load Index.html page to browser
 app.get("/", (req, res) => {
-    let excel_file = JtoE.convertJsonToExcel();
-    res.status(200).send(excel_file);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// sends all imported data
-app.get("/posts", (req, res) => {
-    res.send(importData);
-});
+// associate the multer middleware with the specific provided file storage settings
+const upload = multer({ storage: fileStorageEngine });
 
-
-// accepts an excel file and converts to json
-app.post("/excel_json", (req, res) => {
-    var convertedJson = EtoJ.convertExcelToJson();
-    res.status(200).send(convertedJson);
+// handle a request to upload a json file, process it, then return an excel file
+app.post("/json_excel", upload.single("json"), (req, res) => {
+  console.log(req.file);
+  res.send("JSON upload success");
 });
 
 
-// accepts a json file and converts to excel
-app.post("/json_excel", (req, res) => {
-    var convertedExcel = JtoE.convertJsonToExcel();
-    res.status(200).send(convertedExcel);
-});
+// handle request to upload an excel file, process it, then return a json file
+app.post("/excel_json", upload.single("excel"), (req, res) => {
+    if (isDebugMode) {
+        console.log(req.file);
+    };
+
+    // locate the newest uploaded file
+    var excel_data = req.file.path;
+
+    // convert provided excel to json object
+    const converted_json_object_result = excelToJson({
+        sourceFile: excel_data
+    });
+
+    // convert json object to json file
+    var json_file_result = JSON.stringify(converted_json_object_result);
+    
+    // return the converted json file back to the client
+    res.status(200).send(json_file_result);
+
+    // delete the file from the data storage
+    try {
+        fs.unlinkSync(excel_data);
+        if (isDebugMode) {
+            console.log("File removed:", excel_data);
+        };
+    } catch (err) {
+        console.error(err);
+    }
+
+  });
 
 
+// run server
 app.listen(port, () => {
     console.log(`running on port http://localhost:${port}`);
 });
+
+
+
+
+
+
+// const express = require ("express");
+
+// // middleware for handling / parsing multi-part form data
+// var multer = require('multer');
+
+// const fileStorageEngine = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, "./data")
+//     }, 
+//     filename: (req, file, callback) => {
+//         callback(null, Date.now() + "--" + file.originalname);
+//     },
+// });
+
+// const upload = multer({storage: fileStorageEngine});
+
+// // var isJson = require('is-json');
+
+// // var json2xls = require('json2xls');
+
+// const router = express.Router();
+
+// const app = express();
+
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
+
+// app.use(upload.array());
+
+
+// // var fs = require('fs');
+
+
+// // // import { convertJsonToExcel } from "./xl_json";
+// // const EtoJ = require('./json_xl');
+// // const JtoE = require('./xl_json');
+
+
+// // import data from a json
+// // const importData = require(".//data.json");
+
+// let port = process.env.PORT || 5151;
+
+// // app.get("/", (req, res) => {
+// //     console.log('here');
+// //     let excel_file = JtoE.convertJsonToExcel();
+// //     res.status(200).send(excel_file);
+// // });
+
+// // sends all imported data
+// // app.get("/posts", (req, res) => {
+// //     res.send(importData);
+// // });
+
+
+// // // accepts an excel file and converts to json
+// // app.post("/excel_json", (req, res) => {
+// //     var convertedJson = EtoJ.convertExcelToJson(req, res);
+// //     res.status(200).send(convertedJson);
+// // });
+
+
+// // accepts a json file and converts to excel
+// app.post("/json_excel", upload.single('json'), (req, res) => {
+
+//     console.log(req.file);
+//     console.log(req.body);
+    
+//     var json_data = req.body;
+
+
+
+//     // var excel_output = Date.now() + "output.xlsx"
+
+//     // if(isJson(json_data)){
+//     //     var xls = json2xls(JSON.parse(json_data));
+
+//     //     fs.writeFileSync(excel_output, xls, 'binary');
+
+//     //     result.download(excel_output, (err) => {
+//     //         if(err){
+//     //             fs.unlinkSync(excel_output)
+//     //             result.send("Unable to download Excel file.")
+//     //         }
+//     //         fs.unlinkSync(excel_output)
+//     //     })
+//     // } else {
+//     //     result.send("JSON Data is not valid")
+//     // }
+
+//     res.status(200).send(json_data);
+
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`running on port http://localhost:${port}`);
+// });
--- xl_json.js
+++ xl_json.js
@@ -1,57 +1,82 @@
-// contains functions for processing an Excel worksheet and converting it to a JSON file
+// // // contains functions for processing an Excel worksheet and converting it to a JSON file
 
-var data = "a,b,c\n1,2,3".split("\n").map(function(x) { return x.split(","); });
+// // var data = "a,b,c\n1,2,3".split("\n").map(function(x) { return x.split(","); });
 
-// import this to convert Excel worksheet and converting it to a JSON file
-const XLSX = require('xlsx');
+// // // import this to convert Excel worksheet and converting it to a JSON file
+// // const XLSX = require('xlsx');
 
-function make_book() {
-	var ws = XLSX.utils.aoa_to_sheet(data);
-	var wb = XLSX.utils.book_new();
-	XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
-	return wb;
-}
+// // function make_book() {
+// // 	var ws = XLSX.utils.aoa_to_sheet(data);
+// // 	var wb = XLSX.utils.book_new();
+// // 	XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+// // 	return wb;
+// // }
 
-function get_data(req, res, type) {
-	var wb = make_book();
-	/* send buffer back */
-	res.status(200).send(XLSX.write(wb, {type:'buffer', bookType:type}));
-}
+// // function get_data(req, res, type) {
+// // 	var wb = make_book();
+// // 	/* send buffer back */
+// // 	res.status(200).send(XLSX.write(wb, {type:'buffer', bookType:type}));
+// // }
 
-function get_file(req, res, file) {
-	var wb = make_book();
-	/* write using XLSX.writeFile */
-	XLSX.writeFile(wb, file);
-	res.status(200).send("wrote to " + file + "\n");
-}
-
-
-function load_data(file) {
-	var wb = XLSX.readFile(file);
-	/* generate array of arrays */
-	data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {header:1});
-	console.log(data);
-}
-
-function post_data(req, res) {
-	var keys = Object.keys(req.files), k = keys[0];
-	load_data(req.files[k].path);
-	res.status(200).send("ok\n");
-}
-
-function post_file(req, res, file) {
-	load_data(file);
-	res.status(200).send("ok\n");
-}
+// // function get_file(req, res, file) {
+// // 	var wb = make_book();
+// // 	/* write using XLSX.writeFile */
+// // 	XLSX.writeFile(wb, file);
+// // 	res.status(200).send("wrote to " + file + "\n");
+// // }
 
 
-// export function convertJsonToExcel() {
-//     return "hello";
+// // function load_data(file) {
+// // 	var wb = XLSX.readFile(file);
+// // 	/* generate array of arrays */
+// // 	data = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]], {header:1});
+// // 	console.log(data);
+// // }
+
+// // function post_data(req, res) {
+// // 	var keys = Object.keys(req.files), k = keys[0];
+// // 	load_data(req.files[k].path);
+// // 	res.status(200).send("ok\n");
+// // }
+
+// // function post_file(req, res, file) {
+// // 	load_data(file);
+// // 	res.status(200).send("ok\n");
+// // }
+
+// 'use strict';
+// const excelToJson = require('convert-excel-to-json');
+ 
+// const result = excelToJson({
+//     sourceFile: 'SOME-EXCEL-FILE.xlsx'
+// });
+
+// // export function convertJsonToExcel() {
+// //     return "hello";
+// // }
+
+// module.exports = {
+//     convertJsonToExcel: function (req) {
+// 		var json_data = req.body.json
+
+// 		var excel_output = Date.now() + "output.xlsx"
+
+// 		if(isJson(json_data)){
+// 			var xls = json2xls(JSON.parse(json_data));
+
+// 			fs.writeFileSync(excel_output, cls, 'binary');
+
+// 			result.download(excel_output, (err) => {
+// 				if(err){
+// 					fs.unlinkSync(excel_output)
+// 					result.send("Unable to download Excel file.")
+// 				}
+// 				fs.unlinkSync(excel_output)
+// 			})
+// 		} else {
+// 			result.send("JSON Data is not valid")
+// 		}
+//         console.log('convertJsonToExcel was run');
+//         return "FastRecast just converted Json to Excel";
+//     }
 // }
-
-module.exports = {
-    convertJsonToExcel: function () {
-        console.log('convertJsonToExcel was run');
-        return "FastRecast just converted Json to Excel";
-    }
-}
